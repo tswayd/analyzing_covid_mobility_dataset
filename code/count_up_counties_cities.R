67f5caaf569f902336e41cd74b8712fa537c3167
Delong Tsway <dttsway@usfca.edu>
2020-09-19 14:18:50 -0700

    Shortened variable name

--- code/count_up_counties_cities.R
+++ code/count_up_counties_cities.R
@@ -9,7 +9,7 @@
 # load the package "dplyr"
 library("dplyr")
 
-# We would like to get a count of the number of cities and the 
+# We would like to get a count of the number of cities and the
 # number of counties in a given state that have driving mobility
 # data. The input to this will be the output of the previous script
 # namely the state csv files that we already subset.
@@ -18,8 +18,11 @@ library("dplyr")
 input_file_name <- "output/applemobilitytrends-2020-09-12_Massachusetts.csv"
 state_data <- read.csv(input_file_name)
 
-# starting off with dyplyr chains
-state_data %>%
+# starting off with dplyr chains
+count_cities_counties_by_type <- state_data %>%
   select(geo_type, region, transportation_type) %>%
-  filter(transportation_type == "driving") %>%
-  group_by(geo_type)
+  group_by(geo_type, transportation_type) %>%
+  tally()
+
+write.csv(count_cities_counties_by_type,
+          "output/massachusetts_cities_counties_counts.csv")
