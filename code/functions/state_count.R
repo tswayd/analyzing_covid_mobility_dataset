0ed16e5eb6e98530028a37a1dcad09123cfeeb1b
Delong Tsway <dttsway@usfca.edu>
2020-09-19 23:31:06 -0700

    Altered function to include for loop to iterate over states.

--- code/functions/state_count.R
+++ code/functions/state_count.R
@@ -13,22 +13,21 @@
 # this should also create an output CSV file that is named based on the state
 # that is subset.
 
-state_count <- function(input_file_name,
-                        geo_type,
-                        region,
-                        transportation_type) {
+state_count <- function(input_file_name) {
   # load the package "dplyr"
   library("dplyr")
-  
-  #read in subset data csv
+  # read in subset data csv
   state_data <- read.csv(input_file_name)
-  
   # starting off with dplyr chains
   count_cities_counties_by_type <- state_data %>%
     select(geo_type, region, transportation_type) %>%
     group_by(geo_type, transportation_type) %>%
     tally()
-  
-  write.csv(count_cities_counties_by_type,
-            "output/massachusetts_cities_counties_counts.csv")
+  if (ncol(count_cities_counties_by_type) < 3) {
+    stop("ERROR: file is missing one or two essential columns of data")
   }
+  # write data as csv in output folder
+  write.csv(count_cities_counties_by_type, file = paste0("output/",
+                          tools::file_path_sans_ext(basename(input_file_name)),
+                            "_city_county_count_by_type.csv"))
+}
--- code/functions/subset_mobility_data_to_state.R
+++ code/functions/subset_mobility_data_to_state.R
@@ -9,22 +9,26 @@
 # create a function to subset any US state out of the full dataset
 # this should also create an output CSV file that is named based on the state
 # that is subset.
-subset_mobility_data_to_state <- function(input_file_name,
-                                          state_to_subset) {
+
+subset_mobility_data_to_state <- function(input_file_name,states_to_subset) {
   # read in the complete csv file
   all_covid_data <- read.csv(input_file_name)
-  # subset the data set to only include rows where the sub.region column has
-  # the state name in it.
-  state_data <- all_covid_data[all_covid_data$sub.region == state_to_subset, ]
-  # check that the subset data actually has data in it
-  if (nrow(state_data) == 0) {
-    stop("ERROR: no rows matching given state name. Did you make a typo?")
+  # iterate for loop over vector of state names
+  for (state_to_subset in states_to_subset) {
+    # subset the data set to only include rows where the sub.region column has
+    # the state name in it.
+    state_data <- all_covid_data[all_covid_data$sub.region == state_to_subset, ]
+    # check that the subset data actually has data in it
+    if (nrow(state_data) == 0) {
+      stop("ERROR: no rows matching given state name. Did you make a typo?")
+    }
+    # save the state data to a new csv file in the output directory
+    write.csv(state_data, file = paste0("output/subset_function_outputs/",
+                                        tools::file_path_sans_ext(
+                                          basename(input_file_name)),
+                                        "_",
+                                        state_to_subset,
+                                        ".csv"))
   }
-  # save the state data to a new csv file in the output directory
-  write.csv(state_data, file = paste0("output/",
-                                      tools::file_path_sans_ext(
-                                        basename(input_file_name)),
-                                      "_",
-                                      state_to_subset,
-                                      ".csv"))
+
 }
