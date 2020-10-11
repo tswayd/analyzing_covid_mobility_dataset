69204aafdc3046b84eddf6cedc429de177852386
Delong Tsway <tswayd@gmail.com>
2020-10-11 13:32:20 -0700

    Add outputdir and filename change options

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -18,6 +18,11 @@ then
   exit 1
 fi
 
+state=$1
+state=${state// /_}
+
 Rscript -e "rmarkdown::render('Analysis.Rmd',\
             params = list(state = '$1',\
-            data = '$2'))"
+            data = '$2'),\
+            output_dir = 'output',\
+            output_file = 'Analysis_$state')"
