6a615b48a9f71cd98f88d783f8ca5d19af7e2e7f
Delong Tsway <tswayd@gmail.com>
2020-10-11 12:41:59 -0700

    Run bash script to test and add gitkeep

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -3,6 +3,6 @@
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
 
-Rscript -e "rmarkdown::render('Analysis.Rmd'\
-            params = list(state = 'Arizona',
+Rscript -e "rmarkdown::render('Analysis.Rmd',\
+            params = list(state = 'Washington',\
             data = 'data/raw_data/applemobilitytrends-2020-09-17.csv'))"
