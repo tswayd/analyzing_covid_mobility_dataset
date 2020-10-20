33f30d2a81a0760e7eb7145019070dddeca8bada
Delong Tsway <tswayd@gmail.com>
2020-10-20 20:08:20 +0000

    Initial commit of sequence process script

---
+++ code/process_sequences.sh
@@ -0,0 +1,9 @@
+# Delong Tsway
+# dttsway@dons.usfca.edu
+# 2020-10-18
+
+# Code to calculate and reporte the total number of sequences in this 
+# file. Code to tally up the number of sequences in this data from each 
+# country sorted from largest to smallest. (OPTIONAL) Code to count up 
+# sequences that have more than 29K base pairs and sequences that have 
+# less than 29K base pairs.
