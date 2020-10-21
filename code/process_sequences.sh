dce6b0a0d9dd572569e3f051cd9a085a3114969c
Delong Tsway <tswayd@gmail.com>
2020-10-21 05:20:43 +0000

    Add new code to find sequence number

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -7,10 +7,14 @@
 #!/bin/bash
 
 # code to count total number of sequences in file
-zgrep -c ">" /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz 
+zgrep -c ">" /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz
+
+# this code also works to find the total number of sequences
+bioawk -cfastx 'END{print NR}' /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz 
 
 # code to parse out sequences by country, count by unique value, and then sort in descending order
 bioawk -c fastx '{print $comment}' /blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz | awk 
 '{split($0,a,"|");print a[3]}' | sort | uniq -c | sort -nr
 
-# code to 
+# code to count number of sequences below 29,000 bp and above 29,000 bp
+
