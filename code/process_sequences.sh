1069c6d54e5317921489fc4eaa250ba42426bd62
Delong Tsway <tswayd@gmail.com>
2020-10-22 22:02:08 +0000

    Remove redirects, fix issues

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -11,31 +11,28 @@
 # https://www.cyberciti.biz/faq/run-execute-sh-shell-script/
 # https://askubuntu.com/questions/420981/how-do-i-save-terminal-output-to-a-file
 
-FILE="/blast-db/sars-cov-2-seq-data/2020-10-15_ncbi_sars_cov_2_sequences.fasta.gz"
-
 if [ $# -eq 0 ]
 then
- echo "There is no file path provided"
+ echo "There is no file path provided."
 exit 1
 fi
 
 if [ $# -gt 1 ]
 then
-echo "There is more than the one require argument provided"
+ echo "There is more than the one required argument."
 exit 1
 fi
 # echo statement to describe contents:
-echo "This is some information about the sequences from the Sars-Cov-2 database" > output/SeqSort.txt
+echo "This is some information about the sequences from the Sars-Cov-2 database"
 
 # code to count total number of sequences in file
-echo "Number of sequences in file:" >> output/SeqSort.txt
-bioawk -cfastx 'END{print NR}' $FILE | tee -a output/SeqSort.txt 
+echo "Number of sequences in file:"
+bioawk -c fastx 'END{print NR}' $# 
 
 # code to parse out sequences by country, count by unique value, and then sort in descending order
-echo "Number of sequences by country in descending order:" >> output/SeqSort.txt
-bioawk -c fastx '{print $4}' $FILE | awk '{split($0,a,"|");print a[3]}' | sort | uniq -c | sort -nr | tee -a output/SeqSort.txt
+echo "Number of sequences by country in descending order:"
+bioawk -c fastx '{print $4}' $# | awk '{split($0,a,"|");print a[3]}' | sort | uniq -c | sort -nr
 
 # code to count number of sequences below and above 29,000 bp 
-bioawk -cfastx 'BEGIN{shorter = 0} {if (length($2) < 29000) shorter += 1} END {print "Number of sequences shorter than 29K bp:", shorter}' $FILE | tee -a output/SeqSort.txt
-bioawk -cfastx 'BEGIN{longer = 0} {if (length($2) >= 29000) longer += 1} END {print "Number of sequences equal to or longer than 29K bp:" longer}' $FILE | tee -a output/SeqSort.txt
-
+bioawk -c fastx 'BEGIN{shorter = 0} {if (length($2) < 29000) shorter += 1} END {print "Number of sequences shorter than 29K bp:", shorter}' $#
+bioawk -c fastx 'BEGIN{longer = 0} {if (length($2) >= 29000) longer += 1} END {print "Number of sequences equal to or longer than 29K bp:" longer}' $#
